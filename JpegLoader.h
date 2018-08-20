1c4fc4702c560a1841ca38e4af7723c6f8b504d5
Actuallymoose <37301784+Actuallymoose@users.noreply.github.com>
2018-08-20 16:37:15 +0800

    Added doxygen style comments for documentation to the JpegLoader.h file

--- JpegLoader.h
+++ JpegLoader.h
@@ -4,13 +4,42 @@
 
 #pragma once
 
-//#define DEFAULT_WIDTH 800
-//#define DEFAULT_HEIGHT 800
-
+	/**
+	* @class JpegLoader
+	* @brief  Used to load in .jpg images to use as textures.
+	*
+	*
+	* @author ClickBait Inc
+	* @version 01
+	* @date 20/08/2018 // date is in Australian format.
+	*
+	* @todo
+	*
+	* @bug Program has no bugs.
+	*/
 class JpegLoader
 {
 public:
+		/**
+		* @brief  Binds image to texture
+		*
+		* reads in image by calling LoadImage() then binds that image to a texture to use with opengl.
+		*
+		*@param const char * filename
+		*
+		* @return void
+		*/
 	void BindTexture(const char * filename);
+
+		/**
+		* @brief  Load jpeg image
+		*
+		* reads a jpeg image from file and returns it.
+		*
+		*@param const char * filename
+		*
+		* @return int image
+		*/
 	int LoadImage(const char * filename);
 };
 
