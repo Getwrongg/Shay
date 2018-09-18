beab1777f44c38399a93ad5949dd194863e20594
Shane Martinez <shane.martinez0410@hotmail.com>
2018-09-18 23:46:06 +0800

    edit

--- PortalEngine/objectaudio.h
+++ PortalEngine/objectaudio.h
@@ -9,7 +9,7 @@ public:
 
 	static float FindDistance(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2);
 
-	static int FindVolume(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2, GLdouble startDis);
+	static int FindVolume(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2, GLdouble startDis);//x1, y1 player coordinates, x2, y2 location of where sound is to play from, startDis distance when audio should start
 
 private:
 
